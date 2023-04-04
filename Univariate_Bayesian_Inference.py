17d116a2ef91190d2093624937e42e5887d781c8
Kyriakos Psallidas <kyriakos.psallidas@gmail.com>
2023-04-04 16:24:19 +0300

    Solved problem 3 in R

--- Univariate_Bayesian_Inference.py
+++ Univariate_Bayesian_Inference.py
@@ -23,35 +23,25 @@ class Univariate_Bayesian_Inference():
 
     # We get points to plot the pdf, using evenly seperated points for the x axis that fit the range of the distribution and
     # their corresponding density based on the Gaussian function
-    def generative_distribution_points(self):
-        generative_sigma = (self.generating_var)**(1/2)
+    def generate_distribution_points(self, mean, variance):
+        sigma = (variance)**(1/2)
         # We use 100 observation/target pairs to plot the pdf
-        self.generative_x = np.linspace(
-            self.generating_mean - 3*generative_sigma, self.generating_mean + 3*generative_sigma, 100)
-        self.generative_y = 1/(np.sqrt(2*np.pi)*generative_sigma)*np.exp(-(
-            self.generative_x - self.generating_mean)**2/(2*generative_sigma**2))
-        return self.generative_x, self.generative_y
+        x_points = np.linspace(mean - 3*sigma, mean + 3*sigma, 1000)
+        y_points = 1/(np.sqrt(2*np.pi)*sigma) * \
+            np.exp(-(x_points - mean)**2/(2*variance))
+        return x_points, y_points
 
-    # As above
+    # We get the points to plot the prior distribution
     def prior_distribution_points(self):
-        prior_sigma = (self.prior_var)**(1/2)
-        self.prior_x = np.linspace(
-            self.prior_mean - 3*prior_sigma, self.prior_mean + 3*prior_sigma, 100)
-        self.prior_y = 1/(np.sqrt(2*np.pi)*prior_sigma) * \
-            np.exp(-(self.prior_x - self.prior_mean)**2/(2*prior_sigma**2))
+        self.prior_x, self.prior_y = self.generate_distribution_points(
+            self.prior_mean, self.prior_var)
         return self.prior_x, self.prior_y
 
-    # As above
-    def posterior_distribution_points(self):
-        try:
-            posterior_sigma = (self.posterior_var)**(1/2)
-            self.posterior_x = np.linspace(
-                self.posterior_mean - 3*posterior_sigma, self.posterior_mean + 3*posterior_sigma, 100)
-            self.posterior_y = 1/(np.sqrt(2*np.pi)*posterior_sigma)*np.exp(-(
-                self.posterior_x - self.posterior_mean)**2/(2*posterior_sigma**2))
-        except NameError:
-            print("You cant sample from the posterior without first estimating its paremeters, use the MAP_estimator method first")
-        return self.posterior_x, self.posterior_y
+    # We get the points to plot the generating distribution
+    def generative_distribution_points(self):
+        self.generative_x, self.generative_y = self.generate_distribution_points(
+            self.generating_mean, self.generating_var)
+        return self.generative_x, self.generative_y
 
     def MAP_estimation(self, generated_samples):
         # Posterior mean estimation using equation (1)
@@ -64,30 +54,38 @@ class Univariate_Bayesian_Inference():
             (N*self.prior_var + self.generating_var)
         return self.posterior_mean, self.posterior_var
 
+    # We get the points to plot the posterior distribution
+    def posterior_distribution_points(self):
+        self.posterior_x, self.posterior_y = self.generate_distribution_points(
+            self.posterior_mean, self.posterior_var)
+        return self.posterior_x, self.posterior_y
+
     def plot_pdfs(self, generated_samples):
         plt.figure(figsize=(9, 6))
         plt.title(
-            f"Maximum a posterior estimation of posterior probability of the mean given N={generated_samples.shape[0]} data-points", fontsize=14)
+            f"Maximum a posteriori estimation of posterior probability of the mean given N={generated_samples.shape[0]} data-points", fontsize=14)
+        plt.scatter(generated_samples, np.zeros(generated_samples.shape[0]),
+                    color="tab:red", label="Sample data", alpha=0.5)
         # We plot the distributions based on the points we have calculated
-        plt.plot(self.generative_x, self.generative_y,  color="tab:blue",
-                 label="Generating Distribution", linewidth=3)
-        plt.plot(self.prior_x, self.prior_y,  color="tab:green",
-                 label="Prior Distribution of the mean", linewidth=3)
-        plt.plot(self.posterior_x, self.posterior_y, color="tab:orange",
-                 label="Posterior Distribution of the estimated mean", linewidth=3)
+        plt.plot(self.generative_x, self.generative_y,
+                 color="tab:blue", label="Generating Distribution", linewidth=3)
+        plt.plot(self.prior_x, self.prior_y,
+                 color="tab:green", label="Prior Distribution of the mean", linewidth=3)
+        plt.plot(self.posterior_x, self.posterior_y,
+                 color="tab:orange", label="Posterior Distribution of the estimated mean", linewidth=3)
         # Vertical lines to the denote the means of each distribution
         plt.vlines(7, 0, 4, linestyles='dashed', label="True mean",
-                   color="tab:blue", linewidth=1.5, alpha=0.8)
+                   color="tab:blue", linewidth=1, alpha=0.8)
         plt.vlines(0, 0, 4, linestyles='dashed', label="Prior mean",
-                   color="tab:green", linewidth=1.5, alpha=0.8)
-        plt.vlines(self.posterior_mean, 0, 4, linestyles='dashed',
-                   color="tab:orange", label="Estimated mean", linewidth=1.5, alpha=0.8)
+                   color="tab:green", linewidth=1, alpha=0.8)
+        plt.vlines(self.posterior_mean, 0, 4, linestyles='dashed', color="tab:orange",
+                   label="Estimated posterior mean", linewidth=1, alpha=0.8)
         # Misc settings
-        plt.legend(fontsize=11)
-        plt.ylim(0, 4)
+        plt.legend(fontsize=10)
+        plt.grid(alpha=0.3)
+        plt.ylim(0, 1.75)
         plt.ylabel("Density", fontsize=14)
         plt.xlabel("x", fontsize=14)
         plt.xticks(np.arange(-8, 22, 2))
-        plt.yticks(np.arange(0, 4.0, 0.5))
-        plt.grid()
+        plt.yticks(np.arange(0, 1.75, 0.1))
         plt.show()
